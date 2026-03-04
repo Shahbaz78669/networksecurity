e30e50d3052318359570893e17121b7a825741f7
Shahbaz Khan <shahbazkhan211016@gmail.com>
2026-03-04 16:51:22 +0530

    updated conversion of DataFrame to Dictonary

--- push_data.py
+++ push_data.py
@@ -28,7 +28,7 @@ class ExtractData:
         try:
             data=pd.read_csv(file_path)
             data.reset_index(drop=True,inplace=True)
-            records=list(json.loads(data.T.to_json()).values())
+            records = data.to_dict(orient='records')
             return records
         except Exception as e:
             raise NetworkSecurityException(e,sys)
