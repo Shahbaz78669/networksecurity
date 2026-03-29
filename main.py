e0f785d9751103651aefa8bb44220e7c93718a69
Shahbaz Khan <shahbazkhan211016@gmail.com>
2026-03-29 13:37:34 +0530

    trainer evalaution and mlflow

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig,DataTransformationConfig
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.model_trainer import ModeltrainerConfig,ModelTrainer
 
 
 if __name__ == "__main__":
@@ -29,6 +30,13 @@ if __name__ == "__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info('Data transformation completed')
+
+        logging.info("Model Trianer started")
+        model_trainer_config=ModeltrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
 
 
 
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -14,6 +14,9 @@ TEST_FILE_NAME:str="test.csv"
 # SCHEMA_FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..","..", "data_schema", "schema.yml")
 SCHEMA_FILE_PATH = r"D:\NetworkSecurity\data_schema\schema.yaml"
 
+SAVED_MODEL_DIR=os.path.join("saved_models")
+MODEL_FILE_NAME="model.pkl"
+
 
 ''' DATA Ingestion related constant start with DATA_INGESTION VAR NAME'''
 DATA_INGESTION_COLLECTION_NAME: str="NetworkData"
@@ -46,3 +49,13 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS:dict={
     "n_neighbors":3,
     "weights":"uniform",
 }
+
+DATA_TRANSFORMATION_TRAIN_FILE_PATH:str="train.npy"
+DATA_TRANSFORMATION_TEST_FILE_PATH:str="test.npy"
+
+'''Model trainer related constant start with Model trainer VAR name'''
+MODEL_TRAINER_DIR_NAME:str="modle_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR:str="trained_model"
+MODEL_TRAINED_MODEL_NAME:str="model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE:float=0.6
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD:float=0.05
